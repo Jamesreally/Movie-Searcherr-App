93b492c2dcb98a7fef72e0bfb0abc237b6c88740
JAMESREALLY <James22really@gmail.com>
2023-02-20 07:41:15 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,40 @@
 import "./App.css";
 import { useState } from "react";
 import SearchComponent from "./components/SearchButton";
-import{ErrorBoundary} from 'react-error-boundary';
+import { ErrorBoundary } from "react-error-boundary";
 import Fallback from "../../assignment2/src/component/fallback";
 function App() {
   const [searchInput, setSearchInput] = useState("");
 
-  const [displayfilter, setDisplayfilter] = useState();
   const onChangeHandler = (event) => {
     event.preventDefault();
     setSearchInput(event.target.value);
   };
   return (
     <ErrorBoundary FallbackComponent={Fallback}>
-    <div className="App">
-      <header className="header"><h1>BLOCKBUSTERS</h1></header>
-      <main><span>
-        <input
-          
-          placeholder="Type movie"
-          onChange={onChangeHandler}
-        />
-        </span>
-
-        <SearchComponent
-          setDisplayfilter={setDisplayfilter}
-          displayfilter={displayfilter}
-          searchInput={searchInput}
-        />
-      </main>
-    </div>
-    </ErrorBoundary>  );
+      <div className="App">
+        <header className="header">
+          <h1>BLOCKBUSTERS</h1>
+        </header>
+        <main>
+          <span>
+            <input
+              placeholder="Search Movie......."
+              onChange={onChangeHandler}
+            />
+          </span>
+          <SearchComponent searchInput={searchInput} />
+        </main>
+        <footer className="footer">
+          <img src=""></img>
+          <h2>
+            Dev {"<"}JAMIE{"/>"}
+          </h2>
+          <p>© 2023 Oreofe</p>
+        </footer>
+      </div>
+    </ErrorBoundary>
+  );
 }
 
 export default App;
--- src/components/SearchButton.jsx
+++ src/components/SearchButton.jsx
@@ -1,63 +1,69 @@
 import React, { useEffect, useState } from "react";
-import {useErrorHandler} from 'react-error-boundary'
-const SearchComponent = ({
-  searchInput,
-  displayfilter,
-  setDisplayfilter,}) => {
+import { useErrorHandler } from "react-error-boundary";
 
-  const [movieItem, setMovieItem] = useState([]);
-  const [displayItem, setDisplayItem] = useState();
+const SearchComponent = ({ searchInput }) => {
+  let movie = {
+    Title: "Into The Badlands",
+    Year: "2015",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BNGUzMmNkMWMtNTI4OC00ZGY1LThlZmUtMDI5YjYyZDE2MzE2XkEyXkFqcGdeQXVyMzY0MTE3NzU@._V1_SX300.jpg",
+  };
+
+  let movieTwo = {
+    Title: "Vikings",
+    Year: "2013",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BODk4ZjU0NDUtYjdlOS00OTljLTgwZTUtYjkyZjk1NzExZGIzXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_SX300.jpg",
+  };
+  let movieThree = {
+    Title: "Boyka",
+    Year: "2016",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BNGQ4YjhmNzUtZTI1MC00OTQ3LTk4MTItYWJhNDdmNGZjOWU2XkEyXkFqcGdeQXVyMTA2NjI0MQ@@._V1_SX300.jpg",
+  };
+  let movieFour = {
+    Title: "Money Heist",
+    Year: "2017",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BZDdjYmZlMzQtN2JmZS00N2JkLTg4MGYtMGI3OGVhMWQ1MzMyXkEyXkFqcGdeQXVyMTEzMTI1Mjk3._V1_SX300.jpg",
+  };
+  const [movieItem, setMovieItem] = useState([
+    movie,
+    movieTwo,
+    movieThree,
+    movieFour,
+  ]);
   const [OptionValue, setOptionValue] = useState("");
-   const[loading, setLoading]=useState("")
-  const Errorhandler=useErrorHandler()
-   useEffect(() => {
+
+  useEffect(() => {
     const fetchData = async () => {
       try {
         const res = await fetch(
           `http://www.omdbapi.com/?s=${searchInput}&apikey=2dd35d36`
         );
         const movieData = await res.json();
+
         if (movieData.Search) {
           setMovieItem(movieData.Search);
-          setLoading("");
-          console.log(movieData.Search);
+          console.log(movieData);
         }
       } catch (err) {
         //Errorhandler(err)
-        //ErrorHandle()
+
         console.log("Encountered problem");
       }
     };
 
     fetchData();
   }, [searchInput]);
-/*const ErrorHandle=()=>{
-  return(<div>Pls Enter</div>)
-}*/
+
   const filterYear = () => {
     const releaseDate = movieItem.filter((item) => item.Year === OptionValue);
     console.log(releaseDate);
-    const abb = releaseDate.map((item) => {
-      return <>{item.Title}</>;
-    });
-    setDisplayfilter(abb);
-    setDisplayItem("");
+    if (releaseDate.length > 0) {
+      setMovieItem(releaseDate);
+    }
     console.log(OptionValue);
-  };
-
-  const MovieList = () => {
-    const itemList = movieItem.map((item) => {
-      return ( <div className="movie"><h4>
-          {item.Title}</h4><div>
-          <img  src={item.Poster}></img></div>
-          <p>
-          {item.Year}
-        </p></div>
-      );
-    });
-    setLoading("Loading...")
-    setDisplayItem(itemList);
-    setDisplayfilter("");
   };
 
   const optionHandler = (event) => {
@@ -65,26 +71,40 @@ const SearchComponent = ({
   };
 
   return (
-    < >
-      <button className="buttonsearch" onClick={MovieList}>
-        SEARCH
-      </button><div >
-      <button className="buttonfilter"onClick={filterYear}>fil:Year</button>
-      <select>
-        {movieItem.map((item) => (
-          <>
-            <option className="buttonyear"value={item.Year} onChange={(e)=>setOptionValue(e.target.value)}>
-              {item.Year}
-            </option>
-          </>
-        ))}
-      </select>
-      < div className="movies">
-        {displayItem}
-        {displayfilter}
+    <>
+      <div>
+        <button className="buttonfilter" onClick={filterYear}>
+          fil:Year
+        </button>
+        <select onChange={optionHandler}>
+          <option>Select Year</option>
+          {movieItem.map((item) => (
+            <>
+              <option className="buttonyear" value={item.Year}>
+                {item.Year}
+              </option>
+            </>
+          ))}
+        </select>
+        <div className="movies">
+          {movieItem.map((item) => {
+            return (
+              <div className="movie">
+                <h4>{item.Title}</h4>
+
+                <img
+                  className="image"
+                  src={item.Poster}
+                  alt={item.Title + " image"}
+                ></img>
+
+                <p>{item.Year}</p>
+              </div>
+            );
+          })}
+        </div>
       </div>
-    </div>
-  </>);
+    </>
+  );
 };
 export default SearchComponent;
-
